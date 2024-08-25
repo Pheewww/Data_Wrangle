1f719e0277adbf1a37bd46a1a985d94609679575
Pheewww <umangsingh67899876@gmail.com>
2024-08-25 13:39:00 +0530

    final

--- frontend/dataloom-frontend/src/Components/Table.jsx
+++ frontend/dataloom-frontend/src/Components/Table.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { transformDataset, complexTransformDataset } from "../api";
+import { transformDataset  } from "../api";
 import proptypes from "prop-types";
 
 
